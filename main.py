85cbd20e11061f7f6b651a64bbbc267cd5238b98
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-07-18 16:25:31 -0700

    Working with python exercises.

---
+++ main.py
@@ -0,0 +1,21 @@
+character_name = "Tom"
+character_age = "35"
+isMale = False
+
+print("There once was a man named " + character_name + ", ")
+print("He is " + character_age + " years old.")
+
+print("Giraffe Academy")
+print("Giraffe\nHello")
+
+phrase = "Giraffe Academy"
+print(phrase.upper() + " is cool")
+print(phrase[0])
+print(phrase[1])
+print(phrase[2])
+print(phrase[3])
+
+# Three main data types in python, string, number, and boolean
+
+print(phrase.replace("Giraffe", "Elephant"))
+print(phrase)
